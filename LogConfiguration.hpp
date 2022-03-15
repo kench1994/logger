f37aabe8b1f5dfce6c99dfba079524ff059c310e
kench1994 <kench.h.wong@gmail.com>
2022-03-15 21:30:17 +0800

    init commit

---
+++ LogConfiguration.hpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <limits>
+namespace utils
+{
+    namespace logger
+    {
+        enum class LogLevel : unsigned int{
+            VERBOSE = 2,
+            DEBUG = 3,
+            INFO = 4,
+            WARN = 5,
+            ERROR = 6,
+            //* Log level for XLog#init, printing all logs.
+            ALL = 0,//Integer.MIN_VALUE,
+            //* Log level for XLog#init, printing no log.
+            NONE = (std::numeric_limits<unsigned int>::max)() //Integer.MAX_VALUE
+        };
+
+
+        //能不能用constexpr 或者模板代替?
+        static const std::string getLevelName(LogLevel level)
+        {
+            switch (level) 
+            {
+                case LogLevel::VERBOSE:
+                    return "VERBOSE";
+                case LogLevel::DEBUG:
+                    return "DEBUG";
+                case LogLevel::INFO:
+                    return "INFO";
+                case LogLevel::WARN:
+                    return "WARN";
+                case LogLevel::ERROR:
+                    return "ERROR";
+            }
+            return "";
+        }
+
+//         // Stringify log level with width of 5.
+// static const char *stringifyLogLevel(LogLevel level) {
+//   const char *levelName[] = {"TRAC", "DEBU", "INFO", "WARN", "ERRO", "FATA"};
+//   return levelName[level];
+// }
+
+        class LogConfiguration
+        {
+            public:
+                LogConfiguration(){}
+                ~LogConfiguration(){}
+            private:
+                unsigned int m_uLogLevel;
+                unsigned int m_uStackTraceDepth;
+                bool m_bWithThread;
+
+
+        };
+    }
+}
---
+++ logger.hpp
@@ -0,0 +1,52 @@
+#include <list>
+#include <string>
+#include <memory>
+#include <chrono>
+#define __LOG(loger, level, fmt, ...)\
+{\
+}
+/* Returns microseconds since epoch */
+static uint64_t timestamp_now()
+{
+    //TODO:what's difference?
+//	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
+//todo time-format 学习下boost time
+static void format_timestamp(std::ostream & os, uint64_t timestamp)
+{
+// The next 3 lines do not work on MSVC!
+// auto duration = std::chrono::microseconds(timestamp);
+// std::chrono::high_resolution_clock::time_point time_point(duration);
+// std::time_t time_t = std::chrono::high_resolution_clock::to_time_t(time_point);
+    char miliseconds[7];
+    sprintf(miliseconds, "%03llu", timestamp % 1000);
+    std::time_t time_t = timestamp / 1000;
+    tm* gmtime = std::localtime(&time_t);
+    
+    if(nullptr == gmtime)
+    {
+        os << '[' << miliseconds << ']';
+    }
+    else
+    {
+        char datetime[32];
+        sprintf(datetime, "%d-%02d-%02d %02d:%02d:%02d", gmtime->tm_year+YEAR, gmtime->tm_mon+MONTH,
+            gmtime->tm_mday, gmtime->tm_hour, gmtime->tm_min, gmtime->tm_sec);
+        os << '['<< datetime<< "."<< miliseconds << ']';
+    }
+}
+//我认为log level应该是每个printer单独设置?
+namespace utils
+{
+    class logger
+    {
+        public:
+            logger() {}
+            ~logger() {}
+        protected:
+
+        private:
+            std::list<std::shared_ptr<std::string>> m_vLogInfos;
+    };
+}
